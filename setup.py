cc1dfa6562af849ea265dd9028d2b1af144d3c69
HSwift <18740745+HSwift@users.noreply.github.com>
2022-08-14 03:26:58 +0800

    docs

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup
 
 setup(
     name='pytricks',
-    version='0.1.2',
+    version='0.1.3',
+    author='swift',
+    author_email='qazswift@proton.me',
+    url='https://github.com/HSwift/pytricks',
     py_modules=['pytricks'],
     install_requires=['forbiddenfruit'],
-    description='python no more python'
+    description='A series of python patches based on forbiddenfruit to improve the experience of REPL'
 )
